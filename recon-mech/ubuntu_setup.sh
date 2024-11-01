dcdbbc6c638a414ed2887e88260960869ed4c1f7
Motosploit <Motosploit@gmail.com>
2024-11-01 17:21:58 -0400

    Update ubuntu_setup.sh

--- recon-mech/ubuntu_setup.sh
+++ recon-mech/ubuntu_setup.sh
@@ -1,15 +1,17 @@
 #Setup a ubuntu VM with the latest version
 sudo apt update # Update package list
-#clone the git repo
-git clone https://github.com/Motosploit/ctf_work.git
+#clone the git repo git clone https://github.com/Motosploit/ctf_work.git
 cd ctf_work
 # Install Docker
 sudo apt install -y docker.io 
 # Start and enable Docker to start on boot
 sudo systemctl start docker 
 sudo systemctl enable docker
-#Setup a directory for the server
-mkdir -p ~/recon-mech
+#Setup a directory for the server and copy files into it from git clone that are needed
+#these arent needed if you clone from git as the folder is created for you
+#mkdir -p ~/recon-mech
+#cp app.py recon-mech
+#cp Dockerfile recon-mech
 cd ~/recon-mech
 sudo docker build -t recon-mech .
 sudo docker run -d -p 1337:1337 --name recon-mech recon-mech:latest
